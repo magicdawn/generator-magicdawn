91535ef683263e558612572d174b9fc8830235d9
magicdawn@qq.com <magicdawn@qq.com>
2016-05-15 09:18:36 +0800

    project: use class

--- app/index.js
+++ app/index.js
@@ -3,14 +3,9 @@
 /**
  * module dependencies
  */
-var Base = require('yeoman-generator').Base;
-var inherits = require('util').inherits;
-var _ = require('lodash');
-
-/**
- * do exports
- */
-module.exports = Generator;
+const Base = require('yeoman-generator').Base;
+const inherits = require('util').inherits;
+const _ = require('lodash');
 
 /**
  * Generator Class definition
@@ -21,68 +16,66 @@ module.exports = Generator;
  * # yo magicdawn <file>
  * yo magicdawn jshint
  */
-function Generator() {
-  Base.apply(this, arguments);
+module.exports = class Generator extends Base {
+  constructor() {
+    super();
 
-  this.fileMap = {
-    hint: '.jshintrc',
-    jshint: '.jshintrc',
-    beautify: '.jsbeautifyrc',
-    jsbeautify: '.jsbeautifyrc'
-  };
+    this.fileMap = {
+      hint: '.jshintrc',
+      jshint: '.jshintrc',
+      beautify: '.jsbeautifyrc',
+      jsbeautify: '.jsbeautifyrc'
+    };
 
-  this.sourceRoot(__dirname + '/../');
-  this.fileSrcMap = {
-    '.jshintrc': this.templatePath('app/templates/.jshintrc'),
-    '.jsbeautifyrc': this.templatePath('.jsbeautifyrc')
-  };
+    this.argument('file', {
+      required: false,
+      type: 'String',
+      desc: '生成什么文件'
+    });
 
-  this.argument('file', {
-    required: false,
-    type: 'String',
-    desc: '生成什么文件'
-  });
-}
-inherits(Generator, Base);
-
-/**
- * we starts here
- */
-Generator.prototype.default = function() {
-  var files = this
-    ._getFiles()
-    .filter(x => Boolean(x));
-
-  files.forEach(item => {
-    var src = this.fileSrcMap[item];
-    var dest = this.destinationPath(item);
-    this.fs.copy(src, dest);
-  });
-};
-
-Generator.prototype._getFiles = function() {
-  var files = _.values(this.fileMap);
-  var empty = [];
-
-  // all
-  if (!this.file) {
-    return files;
+    this.sourceRoot(__dirname + '/../');
+    this.fileSrcMap = {
+      '.jshintrc': this.templatePath('app/templates/.jshintrc'),
+      '.jsbeautifyrc': this.templatePath('.jsbeautifyrc')
+    };
   }
 
-  // .
-  if (_.startsWith(this.file, '.')) {
-    if (~files.indexOf(this.file)) {
+  default () {
+    const files = this
+      ._getFiles()
+      .filter(x => Boolean(x));
+
+    files.forEach(item => {
+      const src = this.fileSrcMap[item];
+      const dest = this.destinationPath(item);
+      this.fs.copy(src, dest);
+    });
+  }
+
+  _getFiles() {
+    const files = _.values(this.fileMap);
+    const empty = [];
+
+    // all
+    if (!this.file) {
+      return files;
+    }
+
+    // .
+    if (_.startsWith(this.file, '.')) {
+      if (~files.indexOf(this.file)) {
+        this.log(`file not found: ${ this.file }`);
+        return empty;
+      }
+    }
+
+    //
+    const target = this.fileMap[this.file];
+    if (!target) {
       this.log(`file not found: ${ this.file }`);
       return empty;
     }
-  }
 
-  //
-  var target = this.fileMap[this.file];
-  if (!target) {
-    this.log(`file not found: ${ this.file }`);
-    return empty;
+    return [target];
   }
-
-  return [target];
 };

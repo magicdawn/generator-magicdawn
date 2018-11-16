9ae682c96662a537e7021cfade3764c64eb4b720
magicdawn <magicdawn@qq.com>
2018-11-16 17:19:53 +0800

    fix dot files

--- update_dot_files.sh
+++ update_dot_files.sh
@@ -1,10 +1,10 @@
 rm .eslintrc.yml* .prettierrc*
-wget $DOT_FILES/eslint/2/.eslintrc.yml
+wget $DOT_FILES/.eslintrc.yml
 wget $DOT_FILES/.prettierrc
 
 cd app/templates;
 
 rm .eslintrc.yml* .prettierrc* .travis.yml*
-wget $DOT_FILES/eslint/2/.eslintrc.yml
+wget $DOT_FILES/.eslintrc.yml
 wget $DOT_FILES/.prettierrc
 wget $DOT_FILES/.travis.yml
